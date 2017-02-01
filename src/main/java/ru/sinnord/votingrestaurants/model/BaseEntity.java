30cd3817885a04a9e4c2c66c0d179d01222e4f0f
Sinnord <jctrentonr@gmail.com>
2017-02-01 17:17:22 +0500

    added BaseEntity, NamedEntity and Role(user roles)

---
+++ src/main/java/ru/sinnord/votingrestaurants/model/BaseEntity.java
@@ -0,0 +1,71 @@
+package ru.sinnord.votingrestaurants.model;
+
+import org.hibernate.Hibernate;
+import org.springframework.data.domain.Persistable;
+
+import javax.persistence.*;
+
+/**
+ * Do not manipulate new (transient) entries in HashSet/HashMap without overriding hashCode
+ * http://stackoverflow.com/questions/5031614
+ *
+ * @see org.springframework.data.jpa.domain.AbstractPersistable
+ */
+@MappedSuperclass
+// http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
+@Access(AccessType.FIELD)
+//@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, isGetterVisibility = NONE, setterVisibility = NONE)
+public class BaseEntity implements Persistable<Integer> {
+    public static final int START_SEQ = 100000;
+
+    @Id
+    @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = 100000)
+//    @Column(name = "id", unique = true, nullable = false, columnDefinition = "integer default nextval('global_seq')")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
+    // PROPERTY access for id due to bug: https://hibernate.atlassian.net/browse/HHH-3718
+    @Access(value = AccessType.PROPERTY)
+    protected Integer id;
+
+    public BaseEntity() {
+    }
+
+    protected BaseEntity(Integer id) {
+        this.id = id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return (getId() == null);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !getClass().equals(Hibernate.getClass(o))) {
+            return false;
+        }
+        BaseEntity that = (BaseEntity) o;
+        return getId() != null && getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return (getId() == null) ? 0 : getId();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Entity of type %s with id: %s", getClass().getName(), getId());
+    }
+}
---
+++ src/main/java/ru/sinnord/votingrestaurants/model/Role.java
@@ -0,0 +1,10 @@
+package ru.sinnord.votingrestaurants.model;
+
+/**
+ * User: gkislin
+ * Date: 22.08.2014
+ */
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
